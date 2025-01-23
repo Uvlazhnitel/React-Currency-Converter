ad6a38ba4c6e1c9210a40d608685dc45322102a6
Ilya <ilya.yunkins@gmail.com>
2025-01-23 16:53:07 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; 
+import App from './App'; 
+
+// Создаём корень для рендера
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Рендерим приложение в корень
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
