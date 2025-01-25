0ad6adedcdf66316cf5fc5673c32012faaec3581
Ilya <ilya.yunkins@gmail.com>
2025-01-25 15:02:21 +0200

    Refactor CSS styles for currency converter

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1>Currency Converter</h1>
+    <div className = "converter-container">
+      <div className = "converter">
+      <h1 className = "converter-container__title">Currency Converter</h1>
+        <input type = "number" className = "converter__input" />
+        <input type = "number" className = "converter__input" />
+      </div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import App from './App'; 
+import './App.css';
 
 // Создаём корень для рендера
 const root = ReactDOM.createRoot(document.getElementById('root'));
