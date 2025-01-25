d18e1f9755021d714bb49aae8d20e1ce7feb5968
Ilya <ilya.yunkins@gmail.com>
2025-01-25 15:05:18 +0200

    Refactor CSS styles for currency converter and update input and select widths

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ function App() {
     <div className = "converter-container">
       <div className = "converter">
       <h1 className = "converter-container__title">Currency Converter</h1>
+        <select className = "converter__select">
+          <option value = "USD">USD</option>
+          <option value = "EUR">EUR</option>
+          <option value = "RUB">RUB</option>
+          <option value = "GBP">GBP</option>
+          </select>
+        <input type = "number" className = "converter__input_2" />
         <input type = "number" className = "converter__input" />
-        <input type = "number" className = "converter__input" />
+        <select className = "converter__select_2">
+          <option value = "USD">USD</option>
+          <option value = "EUR">EUR</option>
+          <option value = "RUB">RUB</option>
+          <option value = "GBP">GBP</option>
+          </select>
       </div>
-
     </div>
   );
 }
